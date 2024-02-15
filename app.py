caba1b2d23b9bbcbe9dcfb61af2e6ba08c2c5704
Abhiram <abhi245y@gmail.com>
2024-02-15 22:38:45 +0530

    fixed issue with saving image while submitting
    Did a new approach to grab the image while submitting to solve the issue where the image is different from when it was loaded.

--- app.py
+++ app.py
@@ -2,25 +2,42 @@ from flask import Flask, request, render_template, redirect, jsonify
 import os
 from urllib.parse import urlparse
 import requests
+import base64
+import uuid
 
 app = Flask(__name__, template_folder='template')
 app.config['SECRET_KEY'] = os.urandom(25)
 
+def get_and_cache_image():
+    captcha_image_src = 'https://services.gst.gov.in//services/captcha'
+    image_data = requests.get(captcha_image_src).content
+    return image_data
+
+cached_images = {}
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        captcha_image_src = request.form['captcha_image_src']
-        captcha_input_value = request.form['captcha_input_value']
+        form_data = request.form.to_dict()
+        image_id = form_data['image_id']
+        captcha_input_value = form_data['captcha_input_value']
+
+        cached_image_data = cached_images.get(image_id)
 
         captcha_folder = os.path.join(os.getcwd(), 'captcha')
         if not os.path.exists(captcha_folder):
             os.makedirs(captcha_folder)
 
         with open(os.path.join(captcha_folder, f'{captcha_input_value}.png'), 'wb') as f:
-            f.write(requests.get(captcha_image_src).content)
+            f.write(cached_image_data)
+        return 'Image saved successfully'
+    else:
+        cached_image_data = get_and_cache_image()
+        image_id = str(uuid.uuid4())
+        encoded_image_data = base64.b64encode(cached_image_data).decode('utf-8')
 
-        return 'Form submitted successfully'
-    return render_template('index.html')
+        cached_images[image_id] = cached_image_data
+        return render_template('index.html', image_data=encoded_image_data, image_id=image_id)
 
 @app.route('/get-progress', methods=['POST'])
 def count_capcha_files():
